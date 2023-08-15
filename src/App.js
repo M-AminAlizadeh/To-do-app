d410d1d7700e8fd02a3d2c31bf431ef3c05a693b
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-15 09:07:09 +0330

    Assemble all components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-function App() {
-  return (
-    <></>
-  );
-}
+import Header from './Components/Header';
+import Input from './Components/Input';
+import ToDoItems from './Components/ToDoItems';
+import './style.css';
+
+const App = () => (
+  <main className="todo-container">
+    <Header />
+    <Input />
+    <ToDoItems />
+  </main>
+);
 
 export default App;

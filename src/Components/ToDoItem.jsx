807c502433baddd998e280336a089b3eaad6223d
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-15 09:04:59 +0330

    Build each item initial structure

---
+++ src/Components/ToDoItem.jsx
@@ -0,0 +1,11 @@
+const ToDoItem = () => (
+  <div className="todo-item">
+    <div className="todo-item-name">
+      <input type="checkbox" />
+      <span>task1</span>
+    </div>
+    <img width="20" src="https://img.icons8.com/ios-filled/50/waste.png" alt="waste" />
+  </div>
+);
+
+export default ToDoItem;
